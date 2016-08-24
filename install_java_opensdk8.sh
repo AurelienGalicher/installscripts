7355fe33e8f47901081281aa493190c0d0df66a0
AurelienGalicher <galicher@gmail.com>
2016-08-25 00:41:38 +0200

    Update install_java_opensdk8.sh

--- install_java_opensdk8.sh
+++ install_java_opensdk8.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+sudo -s apt-get update && sudo apt-get install software-properties-common --yes && sudo add-apt-repository ppa:openjdk-r/ppa --yes && sudo apt-get update && sudo apt-get install openjdk-8-jre --yes
 
 echo "java openjdk 8 installation completed"
