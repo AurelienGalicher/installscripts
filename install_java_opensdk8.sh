c37bfe5b83027b6c66128ba289e6e599fc8c4469
AurelienGalicher <galicher@gmail.com>
2016-08-29 14:57:19 +0200

    Delete install_java_opensdk8.sh

--- install_java_opensdk8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -s apt-get update && sudo apt-get install software-properties-common --yes && sudo add-apt-repository ppa:openjdk-r/ppa --yes && sudo apt-get update && sudo apt-get install openjdk-8-jre --yes
-
-echo "java openjdk 8 installation completed"
