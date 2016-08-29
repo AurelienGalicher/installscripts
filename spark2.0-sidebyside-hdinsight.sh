9623d1dee9e9a62827c4b953fd78a94bf3da5a3e
AurelienGalicher <galicher@gmail.com>
2016-08-29 12:27:53 +0200

    Create spark2.0-sidebyside-hdinsight.sh

---
+++ spark2.0-sidebyside-hdinsight.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+newspark="spark-2.0.0-bin-hadoop2.7"
+
+SPARK_DIR="$(readlink -f "/usr/hdp/current/spark-client")"
+SPARK_CONF_DIR="$(readlink -f "/usr/hdp/current/spark-client/conf")"
+CURRENT_DIR=${SPARK_DIR%/spark}
+HADOOP_DIR="$CURRENT_DIR/hadoop"
+HADOOP_YARN_DIR="$CURRENT_DIR/hadoop-yarn"
+$SPARK2_DIR="$CURRENT_DIR/spark-client2"
+
+## Download & Install Binary
+cd "/tmp"
+curl "https://www.apache.org/dist/spark/spark-2.0.0/$newspark.tgz" | tar xzf -
+cd "$newspark"
+rm -r "jars/hadoop"* 
+sudo mkdir $SPARK2_DIR 
+mv "$newspark" "$SPARK2_DIR"
+
+
+# Create symlinks
+sudo ln -s "$SPARK2_DIR/yarn/spark-2.0.0-yarn-shuffle.jar" \
+   "$HADOOP_DIR/lib/spark-yarn-shuffle.jar"
+sudo ln -s $HADOOP_DIR /usr/hdp/current/hadoop
+sudo ln -s $HADOOP_YARN_DIR /usr/hdp/current/hadoop-yarn   
+
+echo "Spark 2.0 installation completed"
